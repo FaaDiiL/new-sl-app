220d746dbec6db3929e344b63dcff0f7162e9d5e
FaaDiiL <fadil.alfarhan@gmail.com>
2021-11-08 05:22:00 +0100

    var to let

--- src/App.js
+++ src/App.js
@@ -704,19 +704,19 @@ const CountDownTimer = () => {
   const [timeLeft, setTimeLeft] = useState(null)
   function parseMillisecondsIntoReadableTime(milliseconds) {
     //Get hours from milliseconds
-    var hours = milliseconds / (1000 * 60 * 60)
-    var absoluteHours = Math.floor(hours)
-    var h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours
+    let hours = milliseconds / (1000 * 60 * 60)
+    let absoluteHours = Math.floor(hours)
+    let h = absoluteHours > 9 ? absoluteHours : '0' + absoluteHours
 
     //Get remainder from hours and convert to minutes
-    var minutes = (hours - absoluteHours) * 60
-    var absoluteMinutes = Math.floor(minutes)
-    var m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes
+    let minutes = (hours - absoluteHours) * 60
+    let absoluteMinutes = Math.floor(minutes)
+    let m = absoluteMinutes > 9 ? absoluteMinutes : '0' + absoluteMinutes
 
     //Get remainder from minutes and convert to seconds
-    var seconds = (minutes - absoluteMinutes) * 60
-    var absoluteSeconds = Math.floor(seconds)
-    var s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds
+    let seconds = (minutes - absoluteMinutes) * 60
+    let absoluteSeconds = Math.floor(seconds)
+    let s = absoluteSeconds > 9 ? absoluteSeconds : '0' + absoluteSeconds
 
     return h + ':' + m + ':' + s
   }
